87876cfdb085e3c9cfc39decb1cb2198a629df1c
bruno <bruno95@naver.com>
2022-06-16 23:56:32 +0900

    modified

--- BFS_prac.py
+++ BFS_prac.py
@@ -6,7 +6,7 @@ def bfs(graph, start, visited):
     visited[start] = 1
     while queue:
         node = queue.popleft()
-        # 이 때 방문체크하면 늦음! > 이미 다른 노드에서 뻗어나가서 방문한 노드가 또 큐에 추가될 수 있음
+        # 이 때 방문체크하면 늦음! > 이미 다른 노드에서 뻗어나가서 방문한 노드가 또 큐에 추가될 수 있음 ex. 7은 2에서 이미 방문했는데 8에서 또 방문함
         # visited[node] = 1
         print(node, end=' ')
         for i in graph[node]:
