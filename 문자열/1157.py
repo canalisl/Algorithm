50e87e8ad3b9822541a1ca355820aff58a448849
bruno <bruno95@naver.com>
2022-07-07 18:59:33 +0900

    BOJ 1157 문자열 5단계 / rstrip()으로 해결

--- 문자열/1157.py
+++ 문자열/1157.py
@@ -4,20 +4,29 @@ input = sys.stdin.readline
 
 
 def findsameletter(word):
-    max_count = 0
-    max_letter = []
-    for letter in word:
-        if word.count(letter) == max_count and letter not in max_letter:
-            max_letter.append(letter)
-        elif word.count(letter) > max_count:
-            max_letter = []
-            max_count = word.count(letter)
-            max_letter.append(letter)
-    if len(max_letter) > 1:
+    letter_cnt = []
+    new_word = list(set(word))
+    for letter in new_word:
+        letter_cnt.append(word.count(letter))
+    if letter_cnt.count(max(letter_cnt)) > 1:
         print('?')
     else:
-        print(max_letter[0])
+        print(new_word[letter_cnt.index(max(letter_cnt))])
+    # 시간초과
+    # max_count = 0
+    # max_letter = []
+    # for letter in word:
+    #     if word.count(letter) == max_count and letter not in max_letter:
+    #         max_letter.append(letter)
+    #     elif word.count(letter) > max_count:
+    #         max_letter = []
+    #         max_count = word.count(letter)
+    #         max_letter.append(letter)
+    # if len(max_letter) > 1:
+    #     print('?')
+    # else:
+    #     print(max_letter[0])
 
 
-word = input().upper()
+word = input().rstrip().upper()
 findsameletter(word)
