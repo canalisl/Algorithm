6822f88864fc40716d94b895e6b76f9f3f1f811a
bruno <bruno95@naver.com>
2022-07-08 21:22:15 +0900

    BOJ 18258 큐, 덱 1단계 / 시간초과 head변수로 해결

--- 큐, 덱/18258.py
+++ 큐, 덱/18258.py
@@ -4,27 +4,31 @@ input = sys.stdin.readline
 
 
 def queueCommand(command):
+    global head
     if command[0] == 'push':
         queue.append(int(command[1]))
     elif command[0] == 'pop':
-        if not queue:
+        if head == len(queue):
             print(-1)
         else:
-            print(queue.pop(0))
+            print(queue[head])
+            # 실제로 값을 제거하지 않고, head만 우측으로 +1만큼 이동
+            head += 1
     elif command[0] == 'size':
-        print(len(queue))
+        print(len(queue) - head)
     elif command[0] == 'empty':
-        if not queue:
+        if head == len(queue):
             print(1)
         else:
             print(0)
     elif command[0] == 'front':
-        if not queue:
+        if head == len(queue):
             print(-1)
         else:
-            print(queue[0])
+            print(queue[head])
     elif command[0] == 'back':
-        if not queue:
+        # 큐가 빈 경우 = head(큐의 인덱스)가 큐의 끝(큐의 길이 - 1)을 초과한 경우 
+        if head == len(queue):
             print(-1)
         else:
             print(queue[len(queue) - 1])
@@ -32,6 +36,8 @@ def queueCommand(command):
 
 N = int(input())
 queue = []
+# 큐의 가장 앞 인덱스를 가리키는 변수
+head = 0
 for _ in range(N):
     command = input().rstrip().split()
     queueCommand(command)
