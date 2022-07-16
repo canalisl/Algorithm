77bad15ffd6306d24e72b6c00a75079f5fa5056f
bruno <bruno95@naver.com>
2022-07-16 23:32:38 +0900

    BOJ 1316 주석 추가

--- 문자열/1316.py
+++ 문자열/1316.py
@@ -6,13 +6,16 @@ input = sys.stdin.readline
 def group_word_check(word):
     global group_word_cnt
     for i in range(len(word)):
+        # 연속된 문자면 건너뛰기
         if i >= 1 and word[i] == word[i - 1]:
             continue
         cnt = word.count(word[i])
+        # 1개인 문자는 따질 필요 X
         if cnt == 1:
             continue
         start = word.find(word[i])
         end = start + cnt - 1
+        # rfind() -> 지정 문자열이 나타나는 '마지막' 위치 반환
         if word.rfind(word[i]) != end:
             return
     else:
