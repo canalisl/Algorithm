5f97d3bcde21c147ffe43ac49d47fbed5df89b9e
bruno <bruno95@naver.com>
2022-07-19 20:53:17 +0900

    visited 안 쓰고 farm도 boolean으로 풀기 / 오히려 더 느림 + 메모리만 소폭 감소

--- 그래프와 순회/1012.py
+++ 그래프와 순회/1012.py
@@ -7,12 +7,12 @@ input = sys.stdin.readline
 
 def count_earthworm_dfs(x, y):
     global cnt
-    visited[y][x] = True
+    farm[y][x] = False
     cnt += 1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < M and 0 <= ny < N and farm[ny][nx] == 1 and not visited[ny][nx]:
+        if 0 <= nx < M and 0 <= ny < N and farm[ny][nx]:
             count_earthworm_dfs(nx, ny)
 
 
@@ -22,19 +22,18 @@ dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 for _ in range(T):
     M, N, K = map(int, input().split())
-    farm = [[0] * M for _ in range(N)]
+    farm = [[False] * M for _ in range(N)]
     for _ in range(K):
         x, y = map(int, input().split())
-        farm[y][x] = 1
+        farm[y][x] = True
     # pprint.pprint(farm)
     # 지렁이 마릿수 초기화
     cnt = 0
     # 각 구역별 배추 포기수
     total_earthworm = []
-    visited = [[False] * M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            if farm[i][j] == 1 and not visited[i][j]:
+            if farm[i][j]:
                 count_earthworm_dfs(j, i)
                 total_earthworm.append(cnt)
                 cnt = 0
