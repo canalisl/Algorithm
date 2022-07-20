214771ca1324e7d39fb6cbfbd83ca9d5ebf4909d
bruno <bruno95@naver.com>
2022-07-20 13:54:30 +0900

    DFS풀이-2 / visited 안쓰고 map을 변경하는 방식

--- 그래프와 순회/2667.py
+++ 그래프와 순회/2667.py
@@ -1,33 +1,40 @@
-import sys, pprint
+import sys
 sys.stdin = open('2667.txt')
 input = sys.stdin.readline
+
 # 상하좌우
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-def find_new_unit():
-    global number
-    for i in range(N):
-        for j in range(N):
-            if map[i][j] == 1 and not visited[i][j]:
-                numbering_complex(j, i)
-                number += 1
 
-    
-def numbering_complex(x, y):
-    # 단지 번호
-    # print(f'ny는 {y}이고, nx는 {x}입니다.' )
-    visited[y][x] = number
+
+def numbering_complex_dfs(x, y):
+    global count
+    # 1인 지역을 지날 때 해당 지점을 0으로 변경
+    map[y][x] = 0
+    # 집의 수 +1
+    count += 1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < N and not visited[ny][nx] and map[ny][nx] == 1:
-            numbering_complex(nx, ny)
-    # print(f'더 이상 갈 곳이 없습니다. nx는 {nx}이고, ny는 {ny}입니다.' )
+        if 0 <= nx < N and 0 <= ny < N and map[ny][nx] == 1:
+            numbering_complex_dfs(nx, ny)
+        
 
-
-number = 1
 N = int(input().rstrip())
 map = [list(map(int, input().rstrip())) for _ in range(N)]
-visited = [[0] * N for _ in range(N)]
-find_new_unit()
-pprint.pprint(visited)
+count = 0
+house_complex = []
+# 방문한 적 없고 '1'인 시작점 찾기
+for i in range(N):
+    for j in range(N):
+        if map[i][j] == 1:
+            numbering_complex_dfs(j, i)
+            # print('DFS 한 사이클 끝')
+            # 한 번 DFS 끝나면 해당 단지 탐색이 끝난 것이므로 count 정산
+            house_complex.append(count)
+            # 다음 단지 탐색 전 집의 수 초기화
+            count = 0
+print(len(house_complex))
+for number in sorted(house_complex):
+    print(number)
+
