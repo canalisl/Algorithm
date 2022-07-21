ab6c6a57e762433def5425a045ecd548bdf8da79
bruno <bruno95@naver.com>
2022-07-22 02:58:00 +0900

    BFS 풀이 완료 / exit() 안 쓰고 풂

--- 그래프와 순회/BFS/7576.py
+++ 그래프와 순회/BFS/7576.py
@@ -3,10 +3,8 @@ import sys, pprint
 sys.stdin = open('7576.txt')
 input = sys.stdin.readline
 
-def ripening_day(x, y):
-    global day
-    queue = deque()
-    queue.append((x, y))
+
+def ripening_day():
     while queue:
         x, y = queue.popleft()
         for k in range(4):
@@ -19,16 +17,35 @@ def ripening_day(x, y):
 
 M, N = map(int, input().split())
 box = [list(map(int, input().split())) for _ in range(N)]
-day = 0
 # 상하좌우
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 cnt = 0
+queue = deque()
 for i in range(N):
     for j in range(M):
+        # 첫 날 안 익은 토마토 갯수 세기
         if box[i][j] == 0:
             cnt += 1
-        if box[i][j] == 1:
-            ripening_day(j, i)
-pprint.pprint(box)
-print(day)
+        elif box[i][j] == 1:
+            # 여기서 바로 BFS 시작하지 않고 익은 토마토 노드를 큐에 추가
+            queue.append((j, i))
+# 처음부터 모든 토마토가 익어있다면
+if cnt == 0:
+    # 0 출력하고 종료
+    print(0)
+else:
+    # 익은 토마토 각각에서 동시에 BFS 시작
+    ripening_day()
+    # 안 익은 토마토가 남아있는지 체크
+    is_unripened = False
+    for k in range(N):
+        for l in range(M):
+            if box[k][l] == 0:
+                is_unripened = True
+                break
+    if is_unripened:
+        print(-1)
+    else:
+        # 익는데 걸리는 날짜 = 박스 최댓값 -1
+        print(max(map(max, box)) - 1)
