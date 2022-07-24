1963dcdeb63aefb4846032227b853d32645d2000
bruno <bruno95@naver.com>
2022-07-25 02:16:15 +0900

    BFS로 풀이 완료

--- 그래프와 순회/1697.py
+++ 그래프와 순회/1697.py
@@ -1,21 +1,22 @@
+from collections import deque
 import sys
 sys.stdin = open('1697.txt')
 input = sys.stdin.readline
 
 
-def find_kids(n, k):
-    global time
-    print(f'n - {n} // k - {k} // time - {time}')
-    if abs(2*n - k) < abs(n - k):
-        # 순간이동 한 위치 ~ 동생 위치 < 지금 위치 ~ 동생위치
-        # 일때만 순간이동 하는게 이득임
-        time += 1
-        find_kids(2*n, k)
-    else:
-        time += abs(n - k)
-
+def find_kids_bfs(x):
+    queue = deque()
+    queue.append(x)
+    while queue:
+        now = queue.popleft()
+        if now == K:
+            return visited[now]
+        for next in (now - 1, now + 1, 2*now):
+            if 0 <= next <= 100000 and not visited[next]:
+                visited[next] = visited[now] + 1
+                queue.append(next)
+            
 
 N, K = map(int, input().split())
-time = 0
-find_kids(N, K)
-print(time)
+visited = [0] * 100001
+print(find_kids_bfs(N))
