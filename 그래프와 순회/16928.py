fabfe9e8224b20e4e213f2432e44523fd0cf0e58
bruno <bruno95@naver.com>
2022-07-27 01:09:10 +0900

    BOJ 16928 뱀과 사다리 게임 / BFS 풀이 완료

--- 그래프와 순회/16928.py
+++ 그래프와 순회/16928.py
@@ -11,24 +11,18 @@ def snake_ladder_game(n):
         now = queue.popleft()
         if now == 100:
             return visited[now]
-        # 사다리 있는 칸이면
-        elif now in ladderX and not visited[ladderY[ladderX.index(now)]]:
-            # 타고 올라가고
-            next = ladderY[ladderX.index(now)]
-            visited[next] = visited[now] + 1
-            queue.append(next)
-        # 뱀 있는 칸이면
-        elif now in snakeU and not visited[snakeV[snakeU.index(now)]]:
-            # 내려오고
-            next = snakeV[snakeU.index(now)]
-            visited[next] = visited[now] + 1
-            queue.append(next)
         else:
             for i in range(6):
                 next = now + dice[i]
-                if 1 <= next <= 100 and not visited[next]:
-                    visited[next] = visited[now] + 1
-                    queue.append(next)
+                if 1 <= next <= 100:
+                    if next in ladderX:
+                        next = ladderY[ladderX.index(next)]
+                    elif next in snakeU:
+                        next = snakeV[snakeU.index(next)]
+                    # 사다리나 뱀을 만나 <이동한 후> 위치의 방문여부가 중요하므로 마지막에 체크
+                    if not visited[next]:
+                        visited[next] = visited[now] + 1
+                        queue.append(next)
             
 
 N, M = map(int, input().split())
