acfd5bfaf8fce94cacffc10c54496dc839a2dd13
bruno <bruno95@naver.com>
2022-07-30 00:12:52 +0900

    BOJ 1966 프린터 큐 / 다시 풀어보기

--- 큐, 덱/1966.py
+++ 큐, 덱/1966.py
@@ -1,29 +1,40 @@
 from collections import deque
 import sys
-# sys.stdin = open('1966.txt')
+sys.stdin = open('1966.txt')
 input = sys.stdin.readline
 
 
-def print_queue(q):
-    global order
-    q[M] = 0
-    print(q)
-    while q:
-        if q[0] == 0:
-            print(order)
-            return
-        elif q[0] == max(q):
-            q.popleft()
+def print_queue():
+    global order, M
+    while queue:
+        # 중요도 최댓값 저장
+        highest = max(queue)
+        # 가장 앞의 값 뽑아서 저장
+        front = queue.popleft()
+        # front 뽑았으므로 타겟 인덱스 앞으로 한 칸 이동
+        M -= 1
+        # 뽑은게 최댓값이라면
+        if front == highest:
+            # 인쇄 진행하므로 순서 +1
             order += 1
+            # M이 -1이라면 인쇄한게 내 타겟이라는 뜻
+            if M < 0:
+                # 순서 출력 후 종료
+                return order
+        # 뽑은게 최댓값이 아니라면
         else:
-            q.append(q[0])
-            q.popleft()
-
+            # 큐의 뒤에 추가
+            queue.append(front)
+            # 타겟이 뽑힌 경우
+            if M < 0:
+                # 타겟 인덱스 제일 뒤로 이동
+                M = len(queue) - 1
+        
 
 T = int(input())
 for _ in range(T):
-    order = 1
+    # 인쇄 순서 초기화
+    order = 0
     N, M = map(int, input().split())
     queue = deque([i for i in map(int, input().split())])
-    fakeQ = deque([i for i in range(1, N + 1)])
-    print_queue(queue)
+    print(print_queue())
