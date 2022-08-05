5bdc955f8407e31bef1c58c2359c028355cf3857
bruno <bruno95@naver.com>
2022-08-06 01:11:32 +0900

    BOJ 정렬 5단계 / 백준 ValueError -> rstrip()으로 해결

--- 정렬/1427.py
+++ 정렬/1427.py
@@ -2,9 +2,8 @@ import sys
 sys.stdin = open('1427.txt')
 input = sys.stdin.readline
 
-N = list(map(int, input()))
+# readline으로 입력 받을 때는 rstrip() 항상 주의!
+# rstrip() 안 붙이니까 백준에서 ValueError 계속 발생!
+N = list(map(int, input().rstrip()))
 N = sorted(N, reverse=True)
-new_str = ''
-for num in N:
-    new_str += str(num)
-print(new_str)
+print("".join(map(str, N)))
