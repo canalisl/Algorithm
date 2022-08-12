14df079de3744fc4dea981dd79f17a10255367d6
bruno <bruno95@naver.com>
2022-08-12 18:38:14 +0900

    BOJ 1181 정렬 9단계 단어 정렬

--- 정렬/1181.py
+++ 정렬/1181.py
@@ -2,13 +2,15 @@ import sys
 sys.stdin = open('1181.txt')
 input = sys.stdin.readline
 
-
-def sort_string():
-    pass
-
-
 N = int(input())
 words = []
 for _ in range(N):
     words.append(input().rstrip())
-print(words)
+no_same_words = list(set(words))
+# 문자열을 사전 순으로 정렬해줌
+no_same_words.sort()
+# 길이 순으로 정렬
+no_same_words.sort(key=len)
+
+for word in no_same_words:
+    print(word)
