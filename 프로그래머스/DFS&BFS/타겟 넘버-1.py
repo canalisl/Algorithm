6beca41db8ae9a00a8b53db7c70d0981ef646772
bruno <bruno95@naver.com>
2022-08-14 18:06:42 +0900

    타겟 넘버 첫번째 풀이-큐를 이용한 BFS

---
+++ 프로그래머스/DFS&BFS/타겟 넘버-1.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    queue.append((numbers[0], 0))
+    queue.append((-1*numbers[0], 0))
+    idx = 0
+    while queue:
+        now, idx = queue.popleft()
+        idx += 1
+        if idx < len(numbers):
+            queue.append((now + numbers[idx], idx))
+            queue.append((now - numbers[idx], idx))
+        else:
+            if now == target:
+                answer += 1
+    return answer
