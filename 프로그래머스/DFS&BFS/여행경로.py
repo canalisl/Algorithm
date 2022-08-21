dd329bbdce6c064201545de508e582508fd29870
bruno <bruno95@naver.com>
2022-08-21 23:48:32 +0900

    여행경로 - DFS / init

---
+++ 프로그래머스/DFS&BFS/여행경로.py
@@ -0,0 +1,11 @@
+def solution(tickets):
+    answer = ["ICN"]
+    travel_dfs(answer, tickets)
+    return answer
+                             
+def travel_dfs(way, tickets):
+    for ticket in tickets:
+        if ticket[0] == way[-1]:
+            way.append(ticket[1])
+            tickets.remove(ticket)
+            travel_dfs(way, tickets)
