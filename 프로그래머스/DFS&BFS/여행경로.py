a0dc132880c128c6527c0aafcba13cf167e03f8d
bruno <bruno95@naver.com>
2022-08-22 20:41:23 +0900

    여행 경로 - stack & DFS 풀이 완료

--- 프로그래머스/DFS&BFS/여행경로.py
+++ 프로그래머스/DFS&BFS/여행경로.py
@@ -1,12 +1,32 @@
+from collections import defaultdict
+
+
 def solution(tickets):
-    answer = ["ICN"]
-    tickets.sort()
-    travel_dfs(answer, tickets)
-    return answer
-                             
-def travel_dfs(way, tickets):
-    for ticket in tickets:
-        if ticket[0] == way[-1]:
-            way.append(ticket[1])
-            tickets.remove(ticket)
-            travel_dfs(way, tickets)
+    answer = []
+    # routes = dict()
+    routes = defaultdict(list)
+    
+    for start, end in tickets:
+        # if start in routes:
+        # defaultdict를 썼기 때문에 key가 존재하는지 체크할 필요 X
+            routes[start].append(end)
+        # else:
+        #     routes[start] = [end]
+            
+    for r in routes.keys():
+        # 도착지 알파벳 역순 정렬 > pop() 했을 때 알파벳순으로 뽑히도록
+        routes[r].sort(reverse=True)
+        
+    stack = ["ICN"]
+    while stack:
+        top = stack[-1]
+        # 아예 출발지 목록에 없음(더 이상 이동 불가능) or 갈 수 있는 도착지 없음(티켓 모두 사용)
+        if (top not in routes) or (not routes[top]):
+            answer.append(stack.pop())
+        else:
+            stack.append(routes[top].pop())
+    
+    return answer[::-1]
+
+tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+print(solution(tickets))
