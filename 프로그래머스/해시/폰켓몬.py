9d14ce8b199f918c744759a24410d10da695668f
bruno <bruno95@naver.com>
2022-08-23 20:10:31 +0900

    폰켓몬 - hashTable(dictionary)를 이용한 풀이 추가

--- 프로그래머스/해시/폰켓몬.py
+++ 프로그래머스/해시/폰켓몬.py
@@ -1,3 +1,4 @@
+# set을 이용한 풀이
 def solution(nums):
     answer = 0
     takeCnt = len(nums) / 2
@@ -5,4 +6,20 @@ def solution(nums):
         answer = takeCnt
     else:
         answer = len(set(nums))
+    return answer
+
+
+# hash(딕셔너리)를 이용한 풀이
+from collections import defaultdict
+
+def solution(nums):
+    answer = 0
+    pokemon = defaultdict(int)
+    for cnt in nums:
+        pokemon[cnt] += 1
+    takeCnt = len(nums) / 2
+    if takeCnt <= len(pokemon):
+        answer = takeCnt
+    else:
+        answer = len(pokemon)
     return answer
