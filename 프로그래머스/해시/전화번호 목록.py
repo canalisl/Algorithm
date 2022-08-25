20609803b78749fa39cc8b7492b6d1c1ab3711a6
bruno <bruno95@naver.com>
2022-08-25 14:50:33 +0900

    전화번호 목록 - 효율성 테스트 통과

--- 프로그래머스/해시/전화번호 목록.py
+++ 프로그래머스/해시/전화번호 목록.py
@@ -19,3 +19,16 @@ def solution(phone_book):
                 answer = False
                 return answer
     return answer
+
+# 효율성테스트 통과
+def solution(phone_book):
+    answer = True
+    # sort()는 기본적으로 문자열화 된 숫자를 "각 자리별 크기 순서대로" 정렬 / 전체 크기, 길이 X
+    # sort(["9", "2365", "23543857395834798", "567", "99999111", "112323", "1"])
+    #  -> ['1', '112323', '23543857395834798', '2365', '567', '9', '99999111']
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
+        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
+            answer = False
+            return answer
+    return answer
