db80624aac061993b48fccaa383afe23143527f5
bruno <bruno95@naver.com>
2022-08-28 01:09:27 +0900

    베스트앨범 - 테스트케이스 2/15 통과.. 왜..

--- 프로그래머스/해시/베스트앨범.py
+++ 프로그래머스/해시/베스트앨범.py
@@ -1,3 +1,4 @@
+# 첫번째 오답
 from collections import defaultdict
 
 def solution(genres, plays):
@@ -10,5 +11,22 @@ def solution(genres, plays):
         value.sort(key=lambda x: (-x, plays.index(x)))
     for key, value in sorted_dict:
         answer.append(plays.index(value[0]))
+        plays[plays.index(value[0])] = -1
         answer.append(plays.index(value[1]))
+    return answer
+
+# 두번째 오답
+from collections import defaultdict
+
+def solution(genres, plays):
+    answer = []
+    playlist = defaultdict(list)
+    for i in range(len(genres)):
+        playlist[genres[i]].append((plays[i], i))
+    sorted_dict = sorted(playlist.items(), key=lambda x: sum(x[1][0]), reverse=True)
+    for key, value in sorted_dict:
+        value.sort(key=lambda x: (-x[0], x[1]))
+    for key, value in sorted_dict:
+        answer.append(value[0][1])
+        answer.append(value[1][1])
     return answer
